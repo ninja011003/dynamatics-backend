fe114e501ea9882c5e98f2ee542f3dfe7e122677
NiranjanSuthagar <niranjan.s@contentstack.com>
2025-11-12 19:46:26 +0530

    added sort node

--- func/sort.py
+++ func/sort.py
@@ -1,2 +1,16 @@
 from typing import List,Dict 
 import pandas as pd
+
+class Sort:
+    def __init__(self,input : pd.DataFrame):
+        self.input = input
+        self.output = None
+        
+    def run(self,field,asc:bool = True):
+        self.output = self.input.sort_values(by=field, ascending=asc)
+        return self.output
+    
+if __name__ == "__main__":
+    sort = Sort(pd.DataFrame({"A": [1,2,3,4,5]}))
+    sort.run()
+    print(sort.output)
